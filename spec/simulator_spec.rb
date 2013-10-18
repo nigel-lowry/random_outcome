370f16df8d866208bff75ca6539d25aac48b7d58
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-10-18 20:57:54 +0100

    Move method

--- spec/simulator_spec.rb
+++ spec/simulator_spec.rb
@@ -1,6 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Simulator do
+
+  RSpec::Matchers.define :give_the_outcome do |outcome|
+    match do |actual|
+      outcome_count = 0
+      trials = 10_000
+      trials.times do
+        outcome_count += 1 if actual.outcome == outcome
+      end
+      outcome_percentage = outcome_count / trials.to_f
+      outcome_percentage.should be_within(0.01).of(@percent)
+    end
+
+    chain :with_the_percentage do |percent|
+      @percent = percent
+    end
+  end
+
   context "p(A) = 10%, p(B) = 90%" do
     before :all do
       @simulator = Simulator.new(a: 0.1, b: 0.9)
@@ -12,25 +29,8 @@ describe Simulator do
       @simulator.outcome.should be
     end
 
-    RSpec::Matchers.define :give_the_outcome do |outcome|
-      match do |actual|
-        outcome_count = 0
-        trials = 10_000
-        trials.times do
-          outcome_count += 1 if actual.outcome == outcome
-        end
-        outcome_percentage = outcome_count / trials.to_f
-        outcome_percentage.should be_within(0.01).of(@percent)
-      end
-
-      chain :with_the_percentage do |percent|
-        @percent = percent
-      end
-    end
-
     it { should give_the_outcome(:a).with_the_percentage(0.1) }
     it { should give_the_outcome(:b).with_the_percentage(0.9) }
     it { should give_the_outcome(:c).with_the_percentage(0.0) }
-
   end
 end
