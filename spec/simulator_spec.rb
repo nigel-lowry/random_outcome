f42688484f34519519565dcf88dd211f5732a834
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-10-18 20:55:30 +0100

    Rename variable

--- spec/simulator_spec.rb
+++ spec/simulator_spec.rb
@@ -15,11 +15,11 @@ describe Simulator do
     RSpec::Matchers.define :give_the_outcome do |outcome|
       match do |actual|
         outcome_count = 0
-        times = 10_000
-        times.times do
+        trials = 10_000
+        trials.times do
           outcome_count += 1 if actual.outcome == outcome
         end
-        outcome_percentage = outcome_count / times.to_f
+        outcome_percentage = outcome_count / trials.to_f
         outcome_percentage.should be_within(0.01).of(@percent)
       end
 
